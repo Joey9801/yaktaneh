9a5416c95b1cb7e762ffe2be04a64e60a21db193
Joe Roberts <joe@jwjr.co.uk>
2017-08-28 22:41:38 +0100

    Simon says solver

--- solver.py
+++ solver.py
@@ -289,9 +289,60 @@ def solve_button(bomb):
         solve_releasing_held_button()
 
 
+def solve_simon_says(bomb):
+    def translate_color(input_color):
+        no_vowel = {
+            Color("R"): Color("U"),
+            Color("U"): Color("R"),
+            Color("G"): Color("Y"),
+            Color("Y"): Color("G")
+        }
+
+        vowel = {
+            Color("R"): Color("U"),
+            Color("U"): Color("Y"),
+            Color("G"): Color("G"),
+            Color("Y"): Color("R")
+        }
+
+        if bomb.vowel_serial_number:
+            return vowel[input_color]
+        else:
+            return no_vowel[input_color]
+
+    # Make sure the serial number is known before starting
+    bomb.serial_number
+
+    Color.print_color_reminder()
+
+    print()
+    print("At each step, give the last color flashed")
+    print("Press <ctrl>+c to finish the module")
+
+    input_colors = list()
+    while True:
+        last_color = ask_question(
+                "What color was the final flash?", Color)
+        if last_color.color_code not in ("R", "U", "G", "Y"):
+            print("Don't know how to do simon says for a " +
+                  "{} flash".format(last_color.color_code))
+            continue
+
+        else:
+            input_colors.append(last_color)
+
+        output_colors = list(map(translate_color, input_colors))
+
+        print("Input sequence  : {}".format(
+            " - ".join(map(str, input_colors))))
+        print("Output sequence : {}".format(
+            " - ".join(map(str, output_colors))))
+
+
 module_solvers = collections.OrderedDict()
 module_solvers["Simple Wires"] = solve_simple_wires
 module_solvers["Button"] = solve_button
+module_solvers["Simon Says"] = solve_simon_says
 
 
 def solve_bomb():
