ea4f34cab87d984919c15bb62311091b43f9c28a
Joe Roberts <joe@jwjr.co.uk>
2017-08-28 22:41:00 +0100

    Button solver

--- solver.py
+++ solver.py
@@ -246,8 +246,52 @@ def solve_simple_wires(bomb):
     input("Press enter to continue...")
 
 
+def solve_button(bomb):
+    Color.print_color_reminder()
+
+    def solve_releasing_held_button():
+        print("Press and hold the button")
+
+        color = ask_question("What color is the lit strip?", Color)
+
+        time_requirement = {
+            "U": 4,
+            "B": 1,
+            "Y": 5,
+            "G": 1,
+            "R": 1,
+            "W": 1,
+        }[color.color_code]
+
+        print("Release the buton when there is a {} in any position".format(time_requirement))
+
+    def immediately_release():
+        print("Press and immediately release the button")
+
+    color = ask_question("What color is the button?", Color)
+
+    text = ask_question("What does the button say?", str)
+    text = text.upper()
+
+    if color == "U" and text == "ABORT":
+        solve_releasing_held_button()
+    elif bomb.num_batteries > 1 and text == "DETONATE":
+        immediately_release()
+    elif color == "W" and bomb.indicators["CAR"]:
+        solve_releasing_held_button()
+    elif bomb.num_batteries > 2 and bomb.indicators["FRK"]:
+        immediately_release()
+    elif color == "Y":
+        solve_releasing_held_button()
+    elif color == "R" and text == "HOLD":
+        immediately_release()
+    else:
+        solve_releasing_held_button()
+
+
 module_solvers = collections.OrderedDict()
 module_solvers["Simple Wires"] = solve_simple_wires
+module_solvers["Button"] = solve_button
 
 
 def solve_bomb():
